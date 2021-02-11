c84fcc69af19385e142116c07b6c20f2f0f6a231
Stephane <stephanevm@gmail.com>
2021-02-11 12:36:17 +0100

    Mailfence integration

---
+++ mailfence.py
@@ -0,0 +1,40 @@
+# Mailfence-specific API Functions
+import json
+import os
+import sys
+import urllib2
+from webob import Request
+
+SUGGEST_URL = 'https://mailfence.com/api/v1/thunderbird/suggest'
+
+def suggest(query_string, client_ip):
+
+    url = SUGGEST_URL + '?' + query_string + '&country=' + client_ip
+    #headers = {SPECIAL_HEADER_TYPE:SPECIAL_HEADER_TEXT}
+
+    #dbg = urllib2.urlopen(url).info().getheader('Content-Type')
+   
+    remote_req = urllib2.Request(url)
+    try:
+        response = urllib2.urlopen(remote_req, timeout=10)
+    except Exception,   e:  
+        #emsg   =   {"succeeded":   false, "errors": {"HTTPError": str(e.reason)}}
+        #print   >> environ['wsgi.errors'], e
+        emsg = {"succeeded": False, "errors": {"Network Error": "Backend timed out."}}
+        return json.dumps(emsg)
+        
+    ctype = response.info().getheader('Content-Type')
+    if ctype.partition(';')[0] == 'application/json':
+        sdata = response.read()
+        sdata = json.loads(sdata)
+            
+        if "offer" in sdata:
+            sdata["quote"] = sdata["offer"]
+            del(sdata["offer"])
+        if "price" in sdata:
+            sdata["price"] = "" + sdata["price"]
+            
+        return json.dumps(sdata)
+
+    emsg = {"succeeded": False, "errors":   {"HTTPError": "Backend returned invalid content type."}}
+    #return json.dumps(emsg)
